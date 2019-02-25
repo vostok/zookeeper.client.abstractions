433d2635ffaf2157375f89754d67cb8b13a6f0cf
kungurtsev <kungurtsev@skbkontur.ru>
2019-02-25 15:47:29 +0500

    CreateModeExtensions

---
+++ Vostok.ZooKeeper.Client.Abstractions/Model/CreateModeExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Vostok.ZooKeeper.Client.Abstractions.Model
+{
+    public static class CreateModeExtensions
+    {
+        public static bool IsSequential(this CreateMode createMode)
+        {
+            return createMode == CreateMode.EphemeralSequential || createMode == CreateMode.PersistentSequential;
+        }
+
+        public static bool IsEphemeral(this CreateMode createMode)
+        {
+            return createMode == CreateMode.Ephemeral || createMode == CreateMode.EphemeralSequential;
+        }
+    }
+}
