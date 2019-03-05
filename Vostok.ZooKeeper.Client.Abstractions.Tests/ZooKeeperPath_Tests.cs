a8b1cba6d3cd040f7fece7decf0dae82aadd2cb5
kungurtsev <kungurtsev@skbkontur.ru>
2019-03-05 16:34:17 +0500

    ZooKeeperPath

---
+++ Vostok.ZooKeeper.Client.Abstractions.Tests/ZooKeeperPath_Tests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Vostok.ZooKeeper.Client.Abstractions.Tests
+{
+    [TestFixture]
+    internal class ZooKeeperPath_Tests
+    {
+        [TestCase("/aaaa", new[] {"aaaa"})]
+        [TestCase("/aaaa/bbb", new[] {"aaaa", "bbb"})]
+        [TestCase("/aaaa/bbb/", new[] {"aaaa", "bbb"})]
+        [TestCase("/aaaa/bbb/c/d/e/f/long_123", new[] {"aaaa", "bbb", "c", "d", "e", "f", "long_123"})]
+        public void Split_should_split_by_slashes(string path, string[] expected)
+        {
+            ZooKeeperPath.Split(path).Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+        }
+
+        [TestCase(new[] {"aaaa"}, "/aaaa")]
+        [TestCase(new[] {"aaaa", "bbb"}, "/aaaa/bbb")]
+        [TestCase(new[] {"/aaaa/", "/bbb"}, "/aaaa/bbb")]
+        [TestCase(new[] {"/aaaa/", "/bbb/"}, "/aaaa/bbb/")]
+        [TestCase(new[] {"/aaaa/", null, "", "/bbb/"}, "/aaaa/bbb/")]
+        [TestCase(new[] {"aaaa", "bbb", "c", "d", "e", "f", "long_123"}, "/aaaa/bbb/c/d/e/f/long_123")]
+        public void Combine_should_combine_by_slashes(string[] segments, string expected)
+        {
+            ZooKeeperPath.Combine(segments).Should().Be(expected);
+        }
+    }
+}
---
+++ Vostok.ZooKeeper.Client.Abstractions/ZooKeeperPath.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using JetBrains.Annotations;
+
+namespace Vostok.ZooKeeper.Client.Abstractions
+{
+    /// <summary>
+    /// ZooKeeper path helper.
+    /// </summary>
+    [PublicAPI]
+    public static class ZooKeeperPath
+    {
+        /// <summary>
+        /// Splits <paramref name="path"/> to segments.
+        /// </summary>
+        public static string[] Split(string path)
+        {
+            return path.Trim('/').Split('/');
+        }
+
+        /// <summary>
+        /// Combines path <paramref name="segments"/>.
+        /// </summary>
+        public static string Combine(string[] segments)
+        {
+            var result = new StringBuilder();
+            for (var i = 0; i < segments.Length; i++)
+            {
+                if (string.IsNullOrEmpty(segments[i]))
+                    continue;
+
+                result.Append("/");
+                result.Append(
+                    i + 1 != segments.Length
+                        ? segments[i].Trim('/')
+                        : segments[i].TrimStart('/'));
+            }
+
+            return result.ToString();
+        }
+    }
+}
