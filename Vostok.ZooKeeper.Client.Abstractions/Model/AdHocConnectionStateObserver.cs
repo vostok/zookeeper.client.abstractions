4f06e2201576bc5437a424210b245148c64f1613
kungurtsev <kungurtsev@skbkontur.ru>
2019-03-26 14:09:45 +0500

    add AdHocConnectionStateObserver

---
+++ Vostok.ZooKeeper.Client.Abstractions/Model/AdHocConnectionStateObserver.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Vostok.ZooKeeper.Client.Abstractions.Model
+{
+    /// <summary>
+    /// Represents a <see cref="IZooKeeperClient"/> <see cref="ConnectionState"/> observer that uses an external delegate to process new states or completion.
+    /// </summary>
+    [PublicAPI]
+    public class AdHocConnectionStateObserver : IObserver<ConnectionState>
+    {
+        [CanBeNull]
+        private readonly Action<ConnectionState> onNext;
+
+        [CanBeNull]
+        private readonly Action onCompleted;
+
+        public AdHocConnectionStateObserver([CanBeNull] Action<ConnectionState> onNext, [CanBeNull] Action onCompleted)
+        {
+            this.onNext = onNext;
+            this.onCompleted = onCompleted;
+        }
+
+        public void OnCompleted()
+        {
+            onCompleted?.Invoke();
+        }
+
+        public void OnNext(ConnectionState value)
+        {
+            onNext?.Invoke(value);
+        }
+
+        public void OnError(Exception error)
+        {
+        }
+    }
+}
---
+++ Vostok.ZooKeeper.Client.Abstractions/Model/AdHocNodeWatcher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace Vostok.ZooKeeper.Client.Abstractions.Model
+{
+    /// <summary>
+    /// Represents a node onEvent that uses an external delegate to process events.
+    /// </summary>
+    [PublicAPI]
+    public class AdHocNodeWatcher : INodeWatcher
+    {
+        [NotNull]
+        private readonly Action<NodeChangedEventType, string> onEvent;
+
+        public AdHocNodeWatcher([NotNull] Action<NodeChangedEventType, string> onEvent)
+        {
+            this.onEvent = onEvent ?? throw new ArgumentNullException(nameof(onEvent));
+        }
+
+        /// <inheritdoc />
+        public Task ProcessEvent(NodeChangedEventType type, string path)
+        {
+            onEvent(type, path);
+            return Task.CompletedTask;
+        }
+    }
+}
