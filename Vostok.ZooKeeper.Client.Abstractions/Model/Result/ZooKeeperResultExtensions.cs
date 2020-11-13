908a5056ae030d8ab07fd21d0d56827b147fbd64
kungurtsev <kungurtsev@skbkontur.ru>
2020-11-13 18:00:39 +0500

    + IsRetriableNetworkError

---
+++ Vostok.ZooKeeper.Client.Abstractions/Model/Result/ZooKeeperResultExtensions.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.Annotations;
+
+namespace Vostok.ZooKeeper.Client.Abstractions.Model.Result
+{
+    [PublicAPI]
+    public static class ZooKeeperResultExtensions
+    {
+        /// <summary>
+        /// Returns whether given <paramref name="result"/> unsuccessful and can be retried later.
+        /// </summary>
+        public static bool IsRetriableNetworkError([NotNull] this ZooKeeperResult result) =>
+            result.Status == ZooKeeperStatus.UnknownError ||
+            result.Status == ZooKeeperStatus.NotConnected ||
+            result.Status == ZooKeeperStatus.ConnectionLoss ||
+            result.Status == ZooKeeperStatus.SessionExpired ||
+            result.Status == ZooKeeperStatus.SessionMoved ||
+            result.Status == ZooKeeperStatus.Timeout;
+    }
+}
